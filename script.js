93bf3039135469e2535767177d0ad255d1874e9d
filpaw <fils.pawlak@gmail.com>
2017-11-08 19:33:39 +0100

    1.4

--- script.js
+++ script.js
@@ -1,74 +1,66 @@
-'use strict';
+"use strict";
 // ZMIANY:
-//
+
 // ADRES POCZTOWY zamiast KOD POCZTOWY
 // zmienione - możliwość wprowadzania > 1 spacja albo >= 0 spacja między elementami
-//
+
 // Możliwość wstawiania więcej niż raz pod rząd tego samego pliku
-//
-// ## Problem z 
-// ## postfix
-// ## usuwa za dużo znaków po działaniu
+
+
+// postfix poprawiony
+// usuwa za dużo znaków po działaniu poprawiony
 
 window.onload = function() {
-    var fileInput = document.getElementById('fileInput');
+    var fileInput = document.getElementById("fileInput");
     var results = new Array();
-    
+
     var signPosition = 0;
     var sign;
 
 // REGULAR EXPRESSIONS
-    var regExp = /[0-9]{2}-[0-9]{3}(\s*[A-ZĆŻŹŁŚ][a-ząćśęńłóźż]+)((\s*|\s*-\s*)[A-ZĆŻŹŁŚ][a-ząćśęńłóźż]+)*\,\s*((ul|[Aa]l)?\s*\.)(\s*[A-ZĆŻŹŁŚ][a-ząćśęńłóźż]+)((\s+|\s*-\s*)[A-ZĆŻŹŁŚ][a-ząćśęńłóźż]+)*\s+[0-9]+[a-z]*(\s*\/\s*[0-9]+[a-z]*)*/g;
+    var regExp = /[0-9]{2}-[0-9]{3}(\s*[A-ZĆŻŹŁŚ][a-ząćśęńłóźż]+)((\s*|\s*-\s*)[A-ZĆŻŹŁŚ][a-ząćśęńłóźż]+)*\s*,\s*((ul|[Aa]l)?\s*\.)(\s*[A-ZĆŻŹŁŚ][a-ząćśęńłóźż]+)((\s+|\s*-\s*)[A-ZĆŻŹŁŚ][a-ząćśęńłóźż]+)*\s+[0-9]+[a-z]*(\s*\/\s*[0-9]+[a-z]*)*/g;
     
-    var changeSpace = / */g;
+    var changeSpace = /\s*/g;
     var changeDot = /\./g;
-    var changeComma = /\,/g;
+    var changeComma = /,/g;
     var changePostalCodeDist = /-[0-9]{3}/g;
     var changeAdressNumberDist = /[a-z][0-9]/g;
     var changeLocalityDist = /[a-z][A-Z]/g;
     
 // show/hide buttons/info
-    $('#resultDisplayArea h1').show();
+    $("#resultDisplayArea h1").show();
     hideSign();
     showTestButtons(1);
     
 // INPUT FILES
-    fileInput.addEventListener('change',
-        function(e) {
+    fileInput.addEventListener("change", function(e) {
+        var files = fileInput.files;
+        var textType = /text/;            
         
-            var files = fileInput.files;
-            var textType = /text/;
-//            var changePostalCodeDistExp = /[0-9]{2}-[0-9]{3}/g;
-            
-        
-            var max2 = files.length;
-        
-//        if(files.length > 2) max2 = 2;
-//        if(results.length == 1) max2 = 1;
-        for(var i = 0; i < max2; i++) {
+        for(var i = 0, length = files.length; i < length; i++) {
             if(files[i].type.match(textType)) {
                 var reader = new FileReader();
                 reader.onload = function(e) {
                     if(!e.target.result.match(regExp)){
                         alert("This set is empty!");
-
                     }
                     else {
-                    var liText = document.createElement('li');
-                    var liResult = document.createElement('li');
+                    var liText = document.createElement("li");
+                    var liResult = document.createElement("li");
                     
-                    var expression = removeDuplicates(e.target.result.match(regExp));
+                    var expression = e.target.result.match(regExp);
                     expression = removeMultiWhitespaces(expression);
+                    expression = removeDuplicates(expression);
                     
                     liText.innerText = e.target.result;
                     liResult.innerText = expression;
                     
-                    $('#resultDisplayArea ul').append(liResult);
+                    $("#resultDisplayArea ul").append(liResult);
                     results.push(expression);
                     
-                    $('#resultDisplayArea h1').hide();
+                    $("#resultDisplayArea h1").hide();
                     if(isLength2()) showSign();
-                    document.getElementById('fileInput').value = "";
+                    document.getElementById("fileInput").value = "";
                     }
                 };
                 reader.readAsText(files[i]);
@@ -77,48 +69,44 @@ window.onload = function() {
     });
     
 // INPUT OPERATORS (SIGN)
-    $('#plus').click(function(e) {
-        if(!isLength2()/* || isOperatorLast()*/) {}
+    $("#plus").click(function(e) {
+        if(!isLength2()) {}
         else {
-            var li = document.createElement('li');
+            var li = document.createElement("li");
             li.innerText = "+";
-            $('#resultDisplayArea ul').append(li);
+            $("#resultDisplayArea ul").append(li);
             results.push($(li).text());
             showInput();
         }
         console.log(results);
-        }
-    )
+    });
     
-    $('#minus').click(function(e) {
-        if(!isLength2()/* || isOperatorLast()*/) {}
+    $("#minus").click(function(e) {
+        if(!isLength2()) {}
         else {
-            var li = document.createElement('li');
+            var li = document.createElement("li");
             li.innerText = "-";
-            $('#resultDisplayArea ul').append(li);
+            $("#resultDisplayArea ul").append(li);
             results.push($(li).text());
             showInput();
         }
         console.log(results);
-
-        }
-    )
+    });
     
-    $('#multi').click(function(e) {
-        if(!isLength2()/* || isOperatorLast()*/) {}
+    $("#multi").click(function(e) {
+        if(!isLength2()) {}
         else {
-            var li = document.createElement('li');
+            var li = document.createElement("li");
             li.innerText = "*";
-            $('#resultDisplayArea ul').append(li);
+            $("#resultDisplayArea ul").append(li);
             results.push($(li).text());
             showInput();
         }
         console.log(results);
-        }
-    )
+    });
     
 // SUBMIT BUTTON
-    $('#submitButton').click(function(e) {
+    $("#submitButton").click(function(e) {
         checkSignPosition();
         if(isTwoNumbersFirst()) {
             switch(sign) {
@@ -131,25 +119,24 @@ window.onload = function() {
             }
         }
         if(!isTwoNumbersFirst()) hideSign();
-        console.log(results);
     });
     
 // TESTS
     var testN = 0;
     
-    $('#test').click(function(e) {
-        checkSignPosition();
+    $("#test").click(function(e) {
         console.log(signPosition);
-        console.log(sign);
+        console.log(results);
+        console.log(document.getElementById("resultDisplayArea"));
     });
     
-    $('#test2').click(function(e) {
+    $("#test2").click(function(e) {
         checkSignPosition();
         console.log(signPosition);
         console.log(results);
     });
     
-    $('#test3').click(function(e) {
+    $("#test3").click(function(e) {
         results[results.length-1].splice(1,1);
     });
 
@@ -157,7 +144,7 @@ window.onload = function() {
     function totalArrays(zbior1, zbior2) {    
         var outcome = new Array();
         var flag;
-        var li = document.createElement('li');
+        var li = document.createElement("li");
         
         for(var i = 0; i < zbior1.length; i++) {
             outcome.push(zbior1[i]);
@@ -176,24 +163,37 @@ window.onload = function() {
         deleteE();
         if(!outcome.length) {
             li.innerText += "This array is empty";
+            if(!results.length) $("#resultDisplayArea h1").show();
+            else {
+                console.log(signPosition);
+                var li2 = li.cloneNode(true);
+                if(signPosition>2){
+                    $("#resultDisplayArea ul li:eq("+(signPosition-3)+")").after(li2);
+//                    $("#resultDisplayArea ul li:eq("+(signPosition-3)+")").append(li2);
+                } else {
+                    $("#resultDisplayArea ul").prepend(li2);
+                }
+                results.splice(signPosition-2,0,outcome);
+            }
         } else {
             li.innerText = outcome;
             var li2 = li.cloneNode(true);
             
             console.log(signPosition);
-            if(signPosition>2){
-                $('#resultDisplayArea ul li:eq('+(signPosition-3)+')').append(li2);
-            } else 
-                $('#resultDisplayArea ul').prepend(li2);
+            if(signPosition>2) {
+                $("#resultDisplayArea ul li:eq("+(signPosition-3)+")").after(li2);
+            } else {
+                $("#resultDisplayArea ul").prepend(li2);
+            }
             results.splice(signPosition-2,0,outcome);          
         }
-        $('#totalDisplayArea ul').append(li); 
+        $("#totalDisplayArea ul").append(li); 
     }
     
     function differenceArrays(zbior1, zbior2) {
         var outcome = new Array();
         var flag;
-        var li = document.createElement('li');
+        var li = document.createElement("li");
         
         for(var i = 0; i < zbior1.length; i++) {
             flag = 0;
@@ -209,16 +209,17 @@ window.onload = function() {
         if(!outcome.length) {
             li.innerText += "This array is empty";
             if(!results.length)
-                $('#resultDisplayArea h1').show();
+                $("#resultDisplayArea h1").show();
             else {
-                
+                console.log(signPosition);
                 var li2 = li.cloneNode(true);
                 if(signPosition>2){
-                    results.splice(signPosition-2,0,outcome);
-                    $('#resultDisplayArea ul li:eq('+(signPosition-3)+')').append(li2);
-                } else
-                    results.splice(0,0,outcome);
-                    $('#resultDisplayArea ul').prepend(li2);
+                    $("#resultDisplayArea ul li:eq("+(signPosition-3)+")").after(li2);
+                } else {
+                    $("#resultDisplayArea ul").prepend(li2);
+                }
+                results.splice(signPosition-2,0,outcome);
+
             }
         } else {
             li.innerText = outcome;
@@ -226,20 +227,20 @@ window.onload = function() {
             
             console.log(signPosition);
             if(signPosition>2){
-                $('#resultDisplayArea ul li:eq('+(signPosition-3)+')').append(li2);
+                $("#resultDisplayArea ul li:eq("+(signPosition-3)+")").after(li2);
                 results.splice(signPosition-2,0,outcome);
             } else {
-                $('#resultDisplayArea ul').prepend(li2);
+                $("#resultDisplayArea ul").prepend(li2);
                 results.splice(0,0,outcome);
             }
             console.log(outcome);
         }
-        $('#differenceDisplayArea ul').append(li);
+        $("#differenceDisplayArea ul").append(li);
     }
     
     function productArrays(zbior1, zbior2) {
         var outcome = new Array();
-        var li = document.createElement('li');
+        var li = document.createElement("li");
         
         for(var i = 0; i < zbior1.length; i++) {
             for(var j = 0; j < zbior2.length; j++) {
@@ -256,27 +257,27 @@ window.onload = function() {
         if(!outcome.length) {
             li.innerText += "This array is empty";
             if(!results.length)
-                $('#resultDisplayArea h1').show();
+                $("#resultDisplayArea h1").show();
             else {
                 
                 var li2 = li.cloneNode(true);
                 if(signPosition>2){
                     results.splice(signPosition-2,0,outcome);
                     console.log(signPosition);
-                    $('#resultDisplayArea ul li:eq('+(signPosition-3)+')').append(li2);
+                    $("#resultDisplayArea ul li:eq("+(signPosition-3)+")").after(li2);
                 } else {
                     results.splice(0,0,outcome);
-                    $('#resultDisplayArea ul').prepend(li2);
+                    $("#resultDisplayArea ul").prepend(li2);
                 }
             }
         } else {
             if(signPosition>2){
-                $('#resultDisplayArea ul li:eq('+(signPosition-3)+')').append(li2);
+                $("#resultDisplayArea ul li:eq("+(signPosition-3)+")").after(li2);
             } else 
-                $('#resultDisplayArea ul').prepend(li2);
+                $("#resultDisplayArea ul").prepend(li2);
             results.splice(signPosition-2,0,outcome);
         }
-        $('#productDisplayArea ul').append(li);   
+        $("#productDisplayArea ul").append(li);   
     }
     
 // REMOVES
@@ -284,12 +285,8 @@ window.onload = function() {
         results.splice(signPosition-2,1);
     }
     
-    function removeFileText() {
-        $('#fileDisplayArea ul').children(':eq(' + signPosition + ')').remove();
-    }
-    
     function removeResultText() {
-        $('#resultDisplayArea ul').children(':eq(' + (signPosition-2) + ')').remove();
+        $("#resultDisplayArea ul li:eq(" + (signPosition-2) + ")").remove();
     }
     
 // OTHER FUNCTIONS
@@ -299,44 +296,44 @@ window.onload = function() {
     }
     
     function isOperatorLast() {
-        if(results[results.length-1] == '+') return  "+";
-        else if(results[results.length-1] == '-') return  "-";
-        else if(results[results.length-1] == '*') return  "*";
+        if(results[results.length-1] == "+") return  "+";
+        else if(results[results.length-1] == "-") return  "-";
+        else if(results[results.length-1] == "*") return  "*";
         return false;
     }
     
     function hideInput() {
-        $('#fileInput').prop('disabled', true);
+        $("#fileInput").prop("disabled", true);
     }
     
     function showInput() {
-        $('#fileInput').prop('disabled', false);
+        $("#fileInput").prop("disabled", false);
     }
     
     function hideSign() {
-        $('.sign').prop('disabled', true);
+        $(".sign").prop("disabled", true);
     }
     
     function showSign() {
-        $('.sign').prop('disabled', false);
+        $(".sign").prop("disabled", false);
     }
     
     function showTestButtons(test) {
-        test == 0 ? $('#testButtons').css('visibility','hidden')
-            : $('#testButtons').css('visibility','visible');
+        test == 0 ? $("#testButtons").css("visibility","hidden")
+            : $("#testButtons").css("visibility","visible");
     }
     
     function isTwoNumbersFirst() {   
         if(!isLength2()){ return 0;}
         else {
-            if(results[0] == '+' &&
-               results[0] == '-' &&
-               results[0] == '*') {
+            if(results[0] == "+" &&
+               results[0] == "-" &&
+               results[0] == "*") {
                 return 0;
             }
-            if(results[1] == '+' &&
-               results[1] == '-' &&
-               results[1] == '*') {
+            if(results[1] == "+" &&
+               results[1] == "-" &&
+               results[1] == "*") {
                 return 0;
             }
             else return 1;
@@ -344,6 +341,7 @@ window.onload = function() {
     }
     
     function deleteE() {
+        checkSignPosition();
             for(var i = 0; i < 3; i++) {
                 removeResult();
                 removeResultText();
@@ -353,9 +351,9 @@ window.onload = function() {
     function checkSignPosition() {
         var count = 2;
         while(count < results.length) {
-            if(results[count] == '+' ||
-               results[count] == '-' ||
-               results[count] == '*') {
+            if(results[count] == "+" ||
+               results[count] == "-" ||
+               results[count] == "*") {
                 sign = results[count];
                 signPosition = count;
                 return signPosition;
@@ -383,17 +381,17 @@ window.onload = function() {
     
     function removeMultiWhitespaces(e) {
         for(var i = 0, length = e.length; i < length; i++) {
-            e[i] = e[i].replace(/ */g, '');
-            e[i] = e[i].replace(/\./g, '. ');
-            e[i] = e[i].replace(/\,/g, ', ');
+            e[i] = e[i].replace(changeSpace, "");
+            e[i] = e[i].replace(changeDot, ". ");
+            e[i] = e[i].replace(changeComma, ", ");
             e[i] = e[i].replace(changePostalCodeDist, function(match) {
                 return match + " ";
             } );
             e[i] = e[i].replace(changeAdressNumberDist, function(match) {
-                return e[i].match(changeAdressNumberDist)[0][0] + ' ' +  e[i].match(changeAdressNumberDist)[0][1];
+                return e[i].match(changeAdressNumberDist)[0][0] + " " +  e[i].match(changeAdressNumberDist)[0][1];
             });
             e[i] = e[i].replace(changeLocalityDist, function(match) {
-                return e[i].match(changeLocalityDist)[0][0] + ' ' +  e[i].match(changeLocalityDist)[0][1];
+                return e[i].match(changeLocalityDist)[0][0] + " " +  e[i].match(changeLocalityDist)[0][1];
             });
         }
         return e;
